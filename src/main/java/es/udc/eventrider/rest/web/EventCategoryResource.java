f1bfb4605ecd51f129231dfd0cf515af37a1fe3c
Cristian Ferreiro Montoiro <cristian.ferreiro@udc.es>
2023-06-10 15:34:29 +0200

    Added categories to admin panel

--- src/main/java/es/udc/eventrider/rest/web/EventCategoryResource.java
+++ src/main/java/es/udc/eventrider/rest/web/EventCategoryResource.java
@@ -1,6 +1,7 @@
 package es.udc.eventrider.rest.web;
 
 import es.udc.eventrider.rest.model.domain.Event;
+import es.udc.eventrider.rest.model.domain.EventCategory;
 import es.udc.eventrider.rest.model.exception.NotFoundException;
 import es.udc.eventrider.rest.model.service.EventCategoryService;
 import es.udc.eventrider.rest.model.service.dto.EventCategoryDTO;
@@ -12,8 +13,11 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/event-categories")
@@ -23,8 +27,29 @@ public class EventCategoryResource {
   private EventCategoryService eventCategoryService;
 
   @GetMapping
-  public List<EventCategoryDTO> findAll(@RequestParam(required = false) String query) {
+  public List<EventCategoryDTO> findAll() {
     return eventCategoryService.findAll();
+  }
+
+  @GetMapping("/published")
+  public List<EventCategoryDTO> findPublished() {
+    List<EventCategoryDTO> categories = eventCategoryService.findAll().stream().filter(
+      categoryDTO -> (categoryDTO.getStatus() == EventCategory.EventCategoryStatus.PUBLISHED)).toList();
+    return categories;
+  }
+
+  @GetMapping("/unreviewed")
+  public List<EventCategoryDTO> findUnreviewed() {
+    List<EventCategoryDTO> categories = eventCategoryService.findAll().stream().filter(
+      categoryDTO -> (categoryDTO.getStatus() == EventCategory.EventCategoryStatus.UNREVIEWED)).toList();
+    return categories;
+  }
+
+  @GetMapping("/rejected")
+  public List<EventCategoryDTO> findRejected() {
+    List<EventCategoryDTO> categories = eventCategoryService.findAll().stream().filter(
+      categoryDTO -> (categoryDTO.getStatus() == EventCategory.EventCategoryStatus.REJECTED)).toList();
+    return categories;
   }
 
   @GetMapping("/{id}")
